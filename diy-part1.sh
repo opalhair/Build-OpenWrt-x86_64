7728b8e508ec9a82fb24077e69fcc7e46aa329c4
opalhair <opalhair@gmail.com>
2020-05-08 10:09:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,10 +9,14 @@
 #=============================================================
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# SSR-Plus
+svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
+
 # Passwall
 mkdir -p package/passwall
 svn co https://github.com/pirately/packages/trunk/Passwall/luci-app-passwall package/luci-app-passwall
