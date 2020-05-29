2b1b467f2e170656d4003f93add41f144c0e98f1
opalhair <opalhair@gmail.com>
2020-05-29 10:47:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,7 @@
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
 
 # Modify Default Password
-# sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root:$1$HpIRxWei$9.QF7hD6aYMMluUbzoTYL/:0:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings
+# sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0/$1$HpIRxWei$9.QF7hD6aYMMluUbzoTYL/:18410/g' package/lean/default-settings/files/zzz-default-settings
 
 echo 'Custom settings'
 sed -i '$i uci set network.lan.ifname="eth1 eth2 eth3"' package/lean/default-settings/files/zzz-default-settings
