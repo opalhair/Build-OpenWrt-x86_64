ae3be22ad31863160b1534235414407a844a5ecd
opalhair <opalhair@gmail.com>
2020-09-06 11:46:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -49,7 +49,4 @@ git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/SmartD
 sed -i "s/option bbr '0'/option bbr '1'/g" package/lean/luci-app-flowoffload/root/etc/config/flowoffload
 
 # uci-app-dockerman
-# mkdir -p package/luci-lib-docker && \
-# wget https://raw.githubusercontent.com/lisaac/luci-lib-docker/master/Makefile -O package/luci-lib-docker/Makefile
-# mkdir -p package/luci-app-dockerman && \
-# wget https://raw.githubusercontent.com/lisaac/luci-app-dockerman/master/Makefile -O package/luci-app-dockerman/Makefile
+git clone https://github.com/lisaac/luci-app-dockerman.git package/openwrt-packages/luci-app-dockerman
