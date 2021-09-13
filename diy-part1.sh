382d94c4c9b5c0873cf4b44e429570a0af06c2f5
opalhair <opalhair@gmail.com>
2021-09-13 21:25:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,16 +17,16 @@
 # sed -i 's/KERNEL_TESTING_PATCHVER:=5.4/KERNEL_TESTING_PATCHVER:=4.19/g' ./target/linux/x86/Makefile  #修改内核版本
 
 # Uncomment a feed source
-# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # advancedsetting (方便上海电信IPTV用户在dnsmasq界面中输入数据，SSH 路由器也有同样效果。）
-svn co https://github.com/opalhair/openwrt-packages/trunk/luci-app-advancedsetting package/luci-app-advancedsetting
+svn co https://github.com/opalhair/openwrt-packages/tree/master/luci-app-advancedsetting package/luci-app-advancedsetting
 
 # SSR-Plus
-svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
+# svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
 
 #passwall
 # sed -i '$a src-git kenzo https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
@@ -44,7 +44,7 @@ sed -i '$a src-git small https://github.com/xiaorouji/openwrt-passwall' feeds.co
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 
 # uci-app-dockerman
-# git clone https://github.com/lisaac/luci-app-dockerman.git package/openwrt-packages/luci-app-dockerman
+ git clone https://github.com/lisaac/luci-app-dockerman.git package/openwrt-packages/luci-app-dockerman
 
 # rClone
 # rm -rf package/lean/luci-app-rclone
