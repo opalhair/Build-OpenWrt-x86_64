345c884e74be3cf5c0d6dc010e3edbdc9c0441c9
opalhair <opalhair@gmail.com>
2022-01-13 10:54:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,7 +23,8 @@
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # advancedsetting (方便上海电信IPTV用户在dnsmasq界面中输入数据，SSH 路由器也有同样效果。）
-svn co https://github.com/opalhair/openwrt-packages/tree/master/luci-app-advancedsetting package/luci-app-advancedsetting
+# svn co https://github.com/opalhair/openwrt-packages/tree/master/luci-app-advancedsetting package/luci-app-advancedsetting
+svn co https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-advanced package/luci-app-advanced
 
 # SSR-Plus
 svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
