c11bec2369be797218935b58872b85e821f6494f
opalhair <opalhair@gmail.com>
2025-07-29 22:20:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # 修改默认管理 IP
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
-
-# 创建自定义 uci-defaults 脚本，设置网口和拨号
+sed -i '1i#define BISON_LOCALEDIR "/usr/share/locale"' openwrt/build_dir/hostpkg/gettext-0.24.1/gettext-tools/src/msgcmp.c# 创建自定义 uci-defaults 脚本，设置网口和拨号
 mkdir -p package/base-files/files/etc/uci-defaults/
 cat <<EOF > package/base-files/files/etc/uci-defaults/99-custom-network
 uci set network.lan.ifname="eth1 eth2 eth3"
