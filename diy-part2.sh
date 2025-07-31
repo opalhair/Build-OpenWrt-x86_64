d9abf71559eb853e587745badf46d6b82c92cfeb
opalhair <opalhair@gmail.com>
2025-07-31 14:30:19 +0800

    Update diy-part2.sh 替换 v2ray-geodata

--- diy-part2.sh
+++ diy-part2.sh
@@ -4,9 +4,13 @@ set -e
 # 修改默认管理 IP
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
 
-# 升级 Golang
+# 替换 golang 包（适配新版依赖）
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+
+# 替换 v2ray-geodata
+rm -rf feeds/packages/net/v2ray-geodata
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
 # 创建 uci-defaults 目录
 mkdir -p package/base-files/files/etc/uci-defaults/
