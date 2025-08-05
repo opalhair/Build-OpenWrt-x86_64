2e81cf033fd85f3b3e6d64ea2d3f062196dc5752
opalhair <opalhair@gmail.com>
2025-08-05 11:40:17 +0800

    替换 v2ray-geodata

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,31 +12,18 @@
 
 
 
-#修改版本内核（下面两行代码前面有#为源码默认最新5.4内核,没#为4.19内核,默认修改X86的，其他机型L大那里target/linux查看，对应修改下面的路径就好）
-# sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=4.19/g' ./target/linux/x86/Makefile  #修改内核版本
-# sed -i 's/KERNEL_TESTING_PATCHVER:=5.4/KERNEL_TESTING_PATCHVER:=4.19/g' ./target/linux/x86/Makefile  #修改内核版本
+
 
 # Uncomment a feed source
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
-# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
 
 
 # SSR-Plus
 # svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
 
-# passwall
-# sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall2.git' feeds.conf.default
-# sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-# sed -i '$a src-git small https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall package/luci-app-passwall
-# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/brook package/brook
-# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/chinadns-ng package/chinadns-ng
-# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/tcping package/tcping
-# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-go package/trojan-go
-# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-plus package/trojan-plus
-# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/xray-core package/xray-core
+
 
 # sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 
@@ -45,17 +32,6 @@
 # AdguardHome
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 
-# uci-app-dockerman
-# git clone https://github.com/lisaac/luci-app-dockerman.git package/openwrt-packages/luci-app-dockerman
-
-# rClone
-# rm -rf package/lean/luci-app-rclone
-# rm -rf package/lean/rclone-webui-react
-# rm -rf package/lean/rclone
-# mkdir -p package/Rclone-OpenWrt
-# svn co https://github.com/ElonH/Rclone-OpenWrt/trunk/luci-app-rclone package/Rclone-OpenWrt/luci-app-rclone
-# svn co https://github.com/ElonH/Rclone-OpenWrt/trunk/rclone-webui-react package/Rclone-OpenWrt/rclone-webui-react
-# svn co https://github.com/ElonH/Rclone-OpenWrt/trunk/rclone package/Rclone-OpenWrt/rclone
 # OpenClash
 src-git openclash https://github.com/vernesong/OpenClash
 # svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
@@ -64,4 +40,12 @@ src-git openclash https://github.com/vernesong/OpenClash
 # Add a feed source
 # 添加 MosDNS 软件源
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+# 替换 Golang
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+# 替换 v2ray-geodata
+rm -rf feeds/packages/net/v2ray-geodata
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
+# 如需移除不需要的包
+rm -rf feeds/packages/utils/mqttled
