69af7a7c239a3085ff941c8757240fb2e63cec73
opalhair <opalhair@gmail.com>
2025-08-05 21:24:31 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,13 +16,7 @@ uci set network.wan6.ifname="eth0"
 uci commit network
 EOF
 # 替换 golang 包（适配新版依赖）
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+# rm -rf feeds/packages/lang/golang
+# git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
 
-# 替换 v2ray-geodata
-rm -rf feeds/packages/net/v2ray-geodata
-find ./ -type f -name "Makefile" | grep v2ray-geodata | xargs rm -f
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-find ./ | grep Makefile | grep mosdns | xargs rm -f
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 chmod +x package/base-files/files/etc/uci-defaults/99-custom-network
