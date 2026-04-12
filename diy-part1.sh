208a949fcf975f8a677bc18f893e98bf80516013
opalhair <opalhair@gmail.com>
2026-04-12 22:44:53 +0800

    Update diy-part1.sh
    替换 Golang 包为 sbwml 的 24.x 高版本，

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# 替换 Golang 包为 sbwml 的 24.x 高版本（确保 ddns-go 编译成功）
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
 
-# 如需添加其他第三方 feed 源，可以在这里追加
+# 只保留这一种 MosDNS 注入方式，防止重复
+echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns.git;v5' >> feeds.conf.default
